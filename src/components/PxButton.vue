4850aef2382564bbe5e323b376f6b30cd514a8c0
Héctor Huerta <heccode@gmail.com>
2020-09-13 20:28:58 -0500

    Navegacion programatica

---
+++ src/components/PxButton.vue
@@ -0,0 +1,18 @@
+<template>
+  <button
+    @click="buttonClick"
+    class="bg-transparent hover:bg-green-500 text-green-700 font-semibold hover:text-white"
+  >
+    <slot></slot>
+  </button>
+</template>
+<script>
+export default {
+  name: 'PxButton',
+  methods: {
+    buttonClick() {
+      this.$emit('custom-click')
+    }
+  }
+}
+</script>
