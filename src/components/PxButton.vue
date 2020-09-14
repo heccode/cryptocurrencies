e5de59f95f42e18191eaa61e6fd89ec669811a89
Héctor Huerta <heccode@gmail.com>
2020-09-13 21:29:09 -0500

    Usando componenets de terceros: Spinners y Chart.js + vue-chartick

--- src/components/PxButton.vue
+++ src/components/PxButton.vue
@@ -1,7 +1,7 @@
 <template>
   <button
     @click="buttonClick"
-    class="bg-transparent hover:bg-green-500 text-green-700 font-semibold hover:text-white"
+    class="bg-transparent hover:bg-green-500 text-green-700 font-semibold hover:text-white py-2 px-2 border border-green-500 hover:border-transparent rounded"
   >
     <slot></slot>
   </button>
